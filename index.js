d436198b0d2f74a442c349d655883db4da058d05
Green-Tea <sittingducks.lorenzo@gmail.com>
2023-08-30 00:58:30 +0700

    init

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const TOKEN = process.env.TOKEN
 
 const LOAD_SLASH = process.argv[2] == "load"
 
-const CLIENT_ID = "924846888929337354"
-const GUILD_ID = "924832445050781766"
+const CLIENT_ID = "890301138031767594"
+const GUILD_ID = "181005441013252099"
 
 const client = new Discord.Client({
     intents: [
@@ -31,7 +31,7 @@ client.player = new Player(client, {
 let commands = []
 
 const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
-for (const file of slashFiles){
+for (const file of slashFiles) {
     const slashcmd = require(`./slash/${file}`)
     client.slashcommands.set(slashcmd.data.name, slashcmd)
     if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
@@ -40,17 +40,17 @@ for (const file of slashFiles){
 if (LOAD_SLASH) {
     const rest = new REST({ version: "9" }).setToken(TOKEN)
     console.log("Deploying slash commands")
-    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
-    .then(() => {
-        console.log("Successfully loaded")
-        process.exit(0)
-    })
-    .catch((err) => {
-        if (err){
-            console.log(err)
-            process.exit(1)
-        }
-    })
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+        .then(() => {
+            console.log("Successfully loaded")
+            process.exit(0)
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(err)
+                process.exit(1)
+            }
+        })
 }
 else {
     client.on("ready", () => {
